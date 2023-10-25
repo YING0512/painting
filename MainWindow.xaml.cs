4a5d163a2aabf3eedab313cca66d4c4865466d7b
YING0512 <5b0g0006@stust.edu.tw>
2023-10-25 19:25:21 +0800

    筆刷.下筆

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,42 @@ namespace painting
     /// </summary>
     public partial class MainWindow : Window
     {
+        string shapeType;
+        Color strokeColor = Colors.Red;
+        Brush strokeBrush = new SolidColorBrush(Colors.Red);
+        int strokeThickness;
+
+        Point start, dest;
         public MainWindow()
         {
             InitializeComponent();
+            strokeColorPicker.SelectedColor = strokeColor;
+        }
+
+        private void ShapeBotton_Click(object sender, RoutedEventArgs e)
+        {
+            var targetRadioBottom = sender as RadioButton;
+            shapeType = targetRadioBottom.Tag.ToString();
+        }
+
+        private void strokeThicknessSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            strokeThickness = Convert.ToInt32(strokeThicknessSlider.Value);
+        }
+
+        private void myCanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            start = e.GetPosition(myCanvas);
+            myCanvas.Cursor = Cursors.Cross;
+            switch(shapeType)
+            {
+                case "Line":
+                    break;
+                case "Rectangle":
+                    break;
+                case "Ellipse":
+                    break;
+            }
         }
     }
 }
